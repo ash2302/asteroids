6b291e78d226675c16cc847527a7e6b7958fdeb8
ash2302 <aabhash.pandey@gmail.com>
2024-12-05 17:11:34 +0000

    Add player ship movement with W/S keys and player rotation with A/D keys

--- main.py
+++ main.py
@@ -12,15 +12,19 @@ def main():
 
     clock = pygame.time.Clock()
     dt = 0
-    player_1 = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+            
+        player.update(dt)
+        
         screen.fill((0, 0, 0))
-        player_1.draw(screen)
+        player.draw(screen)
         pygame.display.flip()
+        
         
         dt = clock.tick(60) / 1000
 
