6f6fd4ded8efb407d9431bde7c42fd2b77d117c0
ash2302 <aabhash.pandey@gmail.com>
2024-12-09 17:46:59 +0000

    Add collision detection to remove asteroids and bullets

--- main.py
+++ main.py
@@ -43,6 +43,13 @@ def main():
                 print("Game over!")
                 sys.exit()
 
+        for asteroid in asteroids:
+            for shot in shots:
+                if shot.does_collide(asteroid):
+                    shot.kill()
+                    asteroid.kill()
+
+
         screen.fill((0, 0, 0))
         
         for obj in drawable:
